97f18feb31a9484a05cfe4eda01667a4aa9d3dc4
Rishabh <rishabhverma.1202@gmail.com>
2024-03-26 16:16:21 +0530

    Setup the project

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import Expenses from "./components/Expenses";
+import "../index.css"
+
+
+function App() {
+    // This is an imperative way of adding HTML element
+    // const para = document.createElement('p');
+    // para.textContent = "This is a paragraph";
+    // document.getElementById('root').append(para);
+
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {
+            id: 'e2',
+            title: 'New TV',
+            amount: 799.49,
+            date: new Date(2021, 2, 12),
+        },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    return (
+        <>
+            <h2>Let's get started!</h2>
+            <Expenses items={expenses} />
+        </>
+
+    )
+}
+
+export default App
---
+++ src/components/NewComponent.jsx
@@ -0,0 +1,10 @@
+
+
+function NewComponent() {
+
+    return (
+        <h1>This is new component</h1>
+    )
+}
+
+export default NewComponent
